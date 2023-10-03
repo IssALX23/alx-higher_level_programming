9a121c7578f5404b67c446330d9f26aac854748b
islam <irhoubari@hotmail.com>
2023-10-03 10:49:15 -0700

    correcte output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for number1 in range(0, 11):
     for number2 in range(0, 10):
         if number1 != number2 and number1 < number2:
-            print("{}{}".format(number1, number2), end=', ')
             if number1 == 8 and number2 == 9:
-                print("\n")
+                print("{}{}".format(number1, number2), end='\n')
+            else:
+                print("{}{}".format(number1, number2), end=', ')
