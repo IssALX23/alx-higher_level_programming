3939dedd61013065da9a4289e33541f788cac3ca
islam <irhoubari@hotmail.com>
2023-10-05 14:39:33 -0700

    checker 3 Requirement fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv)
-    if l == 1:
-        print("0 arguments.")
-    elif l == 2:
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+    count = len(sys.argv)
+    index = 0
+    if count == 1:
+        print("{} arguments.".format(count - 1))
+    elif count == 2:
+        index = 1
+        print("{} argument:".format(count - 1))
+        print("{}: {}".format(index, sys.argv[1]))
     else:
-        count = 0
+        print("{} arguments:".format(count - 1))
+        index = 1
         for item in sys.argv[1:]:
-            count += 1
-        print("{} arguments:".format(count))
-        i = 1
-        for item in sys.argv[1:]:
-            print("{}: {}".format(i, item))
-            i += 1
+            print("{}: {}".format(index, item))
+            index += 1
