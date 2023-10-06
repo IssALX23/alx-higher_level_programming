498069e6213080923108a88556913ac86ea684e1
islam <irhoubari@hotmail.com>
2023-10-05 18:15:36 -0700

    Requirement fixed

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-__import__('sys').stdout.write("{}".format(range[ord('A'):ord('Z'+1)]))
+__import__('sys').stdout.write("{}\n".format(''.join(chr(letter) for letter in range(ord('A'), ord('Z')+1))))
