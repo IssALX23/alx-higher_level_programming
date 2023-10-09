7ee3b9207f005d76e0fa6693973d39d6e48bbe3f
islam <irhoubari@hotmail.com>
2023-10-09 13:16:43 -0700

    update checker 6 - change return value

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return
     new_str = ""
+    if not my_string:
+        return new_str
     for word in my_string:
         for letter in word:
             if letter != 'c' and letter != 'C':
