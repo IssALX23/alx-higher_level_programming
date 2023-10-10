9ba5a8cc0085128acee2774d82ab45189488f553
islam <irhoubari@hotmail.com>
2023-10-09 17:11:44 -0700

    updated checkers

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new_list = []
-    if not my_list:
-        return
+    if my_list is None or not my_list:
+        pass
     if idx < 0 or idx >= len(my_list):
         return my_list
     new_list = my_list.copy()
