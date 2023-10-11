8c47e2fa11436794253e3cf976a7258b221fa255
islam <irhoubari@hotmail.com>
2023-10-11 14:18:20 -0700

    Requirement fail - wrong output

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    found = ""
+    found = []
     for word1 in set_1:
         for word2 in set_2:
             if word1 == word2:
-                found = word1
+                found.append(word1)
     return found
