d0e5bdf87cb76d5bbe6b9791a5e7a1838f62eda6
islam <irhoubari@hotmail.com>
2023-10-23 16:56:25 -0700

    update error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,5 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
+    except (TypeError, ValueError) as msg:
+        print(f"Exception: {msg}")
         return False
