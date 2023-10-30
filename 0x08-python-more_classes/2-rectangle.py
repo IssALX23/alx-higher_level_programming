13f478b374a7f2b5dd2f25cc46365542fad78e30
islam <irhoubari@hotmail.com>
2023-10-30 14:24:56 -0700

    updated

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
         """ set width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -32,7 +32,7 @@ class Rectangle:
         """ set height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -44,4 +44,5 @@ class Rectangle:
         """ returns the rectangle perimeter """
         if self.__width == 0 and self.__height == 0:
             return 0
+
         return self.__width * 2 + self.__height * 2
