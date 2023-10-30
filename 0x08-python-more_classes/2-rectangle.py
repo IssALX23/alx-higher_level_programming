4f8e39f1e55145f0bd85761e517bb411b1b46e79
islam <irhoubari@hotmail.com>
2023-10-30 12:55:44 -0700

    updated infos

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """ initialize class values """
+    """ empty class """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
