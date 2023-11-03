09f9680ec475e3b475e40104a13c7688b0168102
islam <irhoubari@hotmail.com>
2023-11-03 10:23:37 -0700

    update test cases

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -21,3 +21,15 @@ try:
     print(matrix)
 except Exception as e:
     print(e)
+
+print("checker 14")
+try:
+    print(matrix_divided([[4, 20], ["4", 2]], 2))
+except Exception as e:
+    print(e)
+
+print("checker 15")
+try:
+    print(matrix_divided([[4, 20], [4, 3, 2]], 2))
+except Exception as e:
+    print(e)
