4b38dd71330380c0fd47a40d5865e03fecad7a34
islam <irhoubari@hotmail.com>
2023-11-28 04:34:12 -0800

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" define a class of Rectangle """
+
+
+class Rectangle:
+  """ empty class """
+  pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+0x08-python-more_classes
+
+	Task 0: Write an empty class Rectangle that defines a rectangle
+
+
